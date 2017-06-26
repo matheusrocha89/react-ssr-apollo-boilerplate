090cda98534544c12e9a17d4e51d3f89af4b2d26
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2017-06-26 18:44:10 -0300

    Update app component to render the Routes component

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+import Routes from '../../routes';
 
-const App = () => (
+const App = ({ isMobile }) => (
   <div className="App">
-    <h1>Hello World</h1>
+    <Routes isMobile={isMobile} />
   </div>
 );
 
+App.propTypes = {
+  isMobile: PropTypes.bool,
+};
+
+App.defaultProps = {
+  isMobile: false,
+};
 
 export default App;
