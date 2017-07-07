005a4215c2c4d1f88f5cc465802c3f7cbbd0cee5
Matheus Cruz Rocha <matheus.rocha89@gmail.com>
2017-07-07 08:35:42 -0300

    Create NetworkInterface options (#9)
    * Create a HTTPHybridNetworkInterface
    
    Using the example on apollo docs. This interface is Hybrid because it uses by default the batch way to send requests but can let you use the default way too
    
    * Add credits to Apollo team about the Interface
    
    * Moving graphql uri config variable to other file
    
    * Create transport file
    
    This file contains the method that returns the network interface used by graphql
    
    * Update yarn.lock file
    
    * Update yarn.lock file
    
    * Network inferface middlewares folder
    
    * Rename function
    
    This name makes more sense than `createNormalNetworkInterface`

---
+++ src/clients/apollo/http-hybrid-network-interface.js
@@ -0,0 +1,47 @@
+/*
+ * This is the same example on Apollo Docs. Credits to them :)
+ */
+
+import {
+  createBatchingNetworkInterface,
+  createNetworkInterface,
+} from 'apollo-client';
+
+
+class HTTPHybridNetworkInterface {
+
+  constructor(opts) {
+    this.batchedInterface = createBatchingNetworkInterface(opts);
+    this.networkInterface = createNetworkInterface(opts);
+  }
+
+  query(request) {
+    // eslint-disable-next-line no-underscore-dangle
+    if (request.variables && request.variables.__disableBatch) {
+      return this.networkInterface.query(request);
+    }
+
+    return this.batchedInterface.query(request);
+  }
+
+  use(middlewares) {
+    this.networkInterface.use(middlewares);
+    this.batchedInterface.use(middlewares);
+    return this;
+  }
+
+  useAfter(afterwares) {
+    this.networkInterface.useAfter(afterwares);
+    this.batchedInterface.useAfter(afterwares);
+    return this;
+  }
+}
+
+function createHybridNetworkInterface(opts) {
+  return new HTTPHybridNetworkInterface(opts);
+}
+
+export default {
+  HTTPHybridNetworkInterface,
+  createHybridNetworkInterface,
+};
